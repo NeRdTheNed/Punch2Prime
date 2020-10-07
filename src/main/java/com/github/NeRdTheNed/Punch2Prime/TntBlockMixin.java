fa63cd6455d7ea06f2bbd24a29f9bacdd856d752
idwtd <68934871+idwtd@users.noreply.github.com>
2020-10-07 12:21:02 -0700

    Update mixin to use ModifyArg

--- src/main/java/com/github/NeRdTheNed/Punch2Prime/TntBlockMixin.java
+++ src/main/java/com/github/NeRdTheNed/Punch2Prime/TntBlockMixin.java
@@ -1,24 +1,19 @@
 package com.github.NeRdTheNed.Punch2Prime;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.TntBlock;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(TntBlock.class)
-public abstract class TntBlockMixin extends Block {
-
-	//Dummy constructor, I'm not sure if this is how it's supposed to be done or if I'm bad at Mixins.
-	public TntBlockMixin(Settings settings) {
-		super(settings);
-	}
-
-	@Inject(method = "<init>", at = @At("TAIL"))
-	private void init(CallbackInfo info) {
-		setDefaultState(getDefaultState().with(TntBlock.UNSTABLE, true));
-		System.out.println("Punch 2 Prime: Newly placed TNT will now be punchable!");
+public abstract class TntBlockMixin {
+	@ModifyArg(
+		method = "<init>",
+		at = @At(value = "INVOKE", target = "Lnet/minecraft/state/State;with(Lnet/minecraft/state/property/Property;Ljava/lang/Comparable;)Ljava/lang/Object;"),
+		index = 1
+	)
+	private Comparable modifyDefaultValue(Property property, Comparable defaultValue) {
+		return property == TntBlock.UNSTABLE ? true : defaultValue;
 	}
 }
