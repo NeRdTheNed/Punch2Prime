68771cb27ffd5b2b8c90ac0f43b8519f69c57b3f
Ned Loynd <41816363+NeRdTheNed@users.noreply.github.com>
2020-11-24 17:45:47 +1100

    Format code & use logger instead of System.out
    Professional development :tiny_potato:
    Also removes a redundant property from fabric.mod.json

--- src/main/java/com/github/NeRdTheNed/Punch2Prime/TntBlockMixin.java
+++ src/main/java/com/github/NeRdTheNed/Punch2Prime/TntBlockMixin.java
@@ -1,24 +1,25 @@
 package com.github.NeRdTheNed.Punch2Prime;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.TntBlock;
-
+import org.apache.logging.log4j.LogManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.TntBlock;
+
 @Mixin(TntBlock.class)
 public abstract class TntBlockMixin extends Block {
 
-	//Dummy constructor, I'm not sure if this is how it's supposed to be done or if I'm bad at Mixins.
+	// Dummy constructor, I'm not sure if this is how it's supposed to be done or if I'm bad at Mixins.
 	public TntBlockMixin(Settings settings) {
 		super(settings);
 	}
 
 	@Inject(method = "<init>", at = @At("TAIL"))
-	private void init(CallbackInfo info) {
+	private void init(CallbackInfo a) {
 		setDefaultState(getDefaultState().with(TntBlock.UNSTABLE, true));
-		System.out.println("Punch 2 Prime: Newly placed TNT will now be punchable!");
+		LogManager.getLogger("Punch 2 Prime").info("Newly placed TNT will now be punchable!");
 	}
 }
